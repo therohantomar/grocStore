2653752f1a327236a295f8dfca77090ddeb4105c
therohantomar <138652704+therohantomar@users.noreply.github.com>
2024-03-27 18:58:28 +0530

    new route added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import { getUser } from "./Controllers/getUser.mjs";
 import { connection } from "./Services/connection.mjs";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { createUser } from "./Controllers/createUser.mjs";
+import { loginUser } from "./Controllers/loginUser.mjs";
 
 connection();
 
@@ -18,6 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/products", getProducts);
 app.get("/users/:id", getUser);
+app.post("/users", createUser);
+app.post("/users/login", loginUser);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
