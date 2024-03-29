69853f411019de169629549c0dc7b5298f883006
therohantomar <138652704+therohantomar@users.noreply.github.com>
2024-03-29 10:37:31 +0530

    testing purpose

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getProducts } from "../Controllers/getProducts.mjs";
-import { getUser } from "../Controllers/getProducts.mjs";
+// import { getProducts } from "../Controllers/getProducts.mjs";
+// import { getUser } from "../Controllers/getProducts.mjs";
 import { connection } from "../Services/connection.mjs";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { createUser } from "../Controllers/getProducts.mjs";
-import { loginUser } from "../Controllers/getProducts.mjs";
-import { forgetPassword } from "../Controllers/getProducts.mjs";
+// import { createUser } from "../Controllers/getProducts.mjs";
+// import { loginUser } from "../Controllers/getProducts.mjs";
+// import { forgetPassword } from "../Controllers/getProducts.mjs";
 
 connection();
 const app = express();
@@ -18,11 +18,13 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/products", getProducts);
-app.get("/users/:id", getUser);
-app.post("/users", createUser);
-app.post("/users/login", loginUser);
-app.post("/users/forgetpassword", forgetPassword);
+app.get("/products", (req, res) => {
+  res.json("hello");
+});
+// app.get("/users/:id", getUser);
+// app.post("/users", createUser);
+// app.post("/users/login", loginUser);
+// app.post("/users/forgetpassword", forgetPassword);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
