7f8a9821835a3654f55332b9cffbefd570a33e2e
therohantomar <138652704+therohantomar@users.noreply.github.com>
2024-03-29 10:42:00 +0530

    route changed

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/products", (req, res) => {
+app.get("/", (req, res) => {
   res.json("hello");
 });
 // app.get("/users/:id", getUser);
