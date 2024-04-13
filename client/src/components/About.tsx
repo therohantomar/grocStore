af6e25e0f0540ccb674e89cb195276b56404334f
Ritikabhandari27000 <ritikbhandari2701@gmail.com>
2024-04-13 10:40:42 +0530

    Aboout Page Added

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,5 +1,33 @@
+
+
 const About = () => {
-  return <div className="mt-10">About</div>;
+  return (
+    <div className="bg-gradient-to-b mb-10 from-green-400 to-blue-500 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-12 rounded-lg  w-full max-w-4xl">
+        <h1 className="text-4xl font-semibold mb-8 text-center  text-gray-600 capitalize">About grocStore</h1>
+        
+        <p className="text-lg text-gray-700 mb-6">
+          Welcome to grocStore, your trusted destination for all your grocery needs. We are committed to providing our customers with a seamless shopping experience, offering a wide range of high-quality groceries at the most affordable prices.
+        </p>
+
+        <p className="text-lg text-gray-700 mb-6">
+          At grocStore, we believe that everyone deserves access to fresh, nutritious, and delicious food. That's why we source our products from trusted suppliers and ensure that they meet our stringent quality standards.
+        </p>
+
+        <p className="text-lg text-gray-700 mb-6">
+          Our dedicated team of professionals works tirelessly to ensure that our shelves are always stocked with a diverse selection of products to cater to your every need. Whether you're looking for fresh fruits and vegetables, pantry staples, or gourmet treats, you'll find it all at grocStore.
+        </p>
+
+        <p className="text-lg text-gray-700 mb-6">
+          We are passionate about supporting our local community and strive to offer products from local farmers and producers whenever possible. By choosing grocStore, you're not only getting great value but also supporting local businesses and sustainable practices.
+        </p>
+
+        <p className="text-lg text-gray-700 mb-6">
+          Thank you for choosing grocStore. We look forward to serving you and making your grocery shopping experience enjoyable and hassle-free!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
