f9006f57345a0acd66f526f36fc2b5568f0d1ec9
therohantomar <138652704+therohantomar@users.noreply.github.com>
2024-05-02 11:17:25 +0530

    authentication

--- server/Controllers/reAuthenticate.mjs
+++ server/Controllers/reAuthenticate.mjs
@@ -16,18 +16,18 @@ export const reAuthenticate = async (req, res) => {
     const user = jwt.verify(authtoken, process.env.JWT_SECRET);
 
     // Check if the token is expired
-    const decoded = jwt.decode(authtoken);
-    if (decoded.exp < Date.now() / 1000) {
-      return res.status(402).json({ status: 401, error: "Token expired" });
-    }
+    // const decoded = jwt.decode(authtoken);
+    // if (decoded.exp < Date.now() / 1000) {
+    //   return res.status(402).json({ status: 401, error: "Token expired" });
+    // }
 
     // Find the user with the given id
     const foundUser = await users.findById(user.id);
 
     // Check if the user is valid
-    if (!foundUser) {
-      return res.status(400).json({ status: 400, error: "Invalid user" });
-    }
+    // if (!foundUser) {
+    //   return res.status(400).json({ status: 400, error: "Invalid user" });
+    // }
 
     // Send the user as a JSON response
     res.status(200).json({ status: 200, user: foundUser });
